51f1019ed79258b531064db3adc40acbbc49a267
Mark Fuentes <osodg@yahoo.com>
2024-01-01 09:37:32 -0500

    Write test for listing users.

--- app/core/models.py
+++ app/core/models.py
@@ -33,7 +33,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-  """User i the system."""
+  """User is in the system."""
   email = models.EmailField(max_length=255, unique=True)
   name = models.CharField(max_length=255)
   is_active = models.BooleanField(default=True)
