572fe085a40854e9b8090fe7fbeed023b5cd5375
Mark Fuentes <osodg@yahoo.com>
2024-01-06 10:07:37 -0500

    Fix typos, correct namespace 'init', and some spacing issues.

--- app/app/calc.py
+++ app/app/calc.py
@@ -8,5 +8,5 @@ def add(x, y):
   return x + y
 
 def subtract(x, y):
-  """Subtract y from x and return result."""
-  return x - y 
+  """Subtract x from y and return result."""
+  return y - x
--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,8 @@
 """
-Database models
+Database models.
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
   AbstractBaseUser,
   BaseUserManager,
   PermissionsMixin,
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
     return user
 
   def create_superuser(self, email, password):
-    """Create and return a new superuser"""
+    """Create and return a new superuser."""
     user = self.create_user(email, password)
     user.is_staff = True
     user.is_superuser = True
