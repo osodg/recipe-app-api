a1e25343dbdb874d8bcd690c62ad380ef87c6b8b
Mark Fuentes <osodg@yahoo.com>
2024-01-08 10:28:19 -0500

    Write test for recipe model and implement recipe model.

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
   AbstractBaseUser,
@@ -33,12 +34,28 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-  """User in the system."""
-  email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
