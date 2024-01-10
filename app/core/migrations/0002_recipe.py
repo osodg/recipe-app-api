9a2336a526bc0f9084dc440fdcc29837bde2dbf4
Mark Fuentes <osodg@yahoo.com>
2024-01-09 20:27:07 -0500

    Implement recipe listing API.

--- app/core/migrations/0002_recipe.py
+++ app/core/migrations/0002_recipe.py
@@ -16,8 +16,8 @@ class Migration(migrations.Migration):
             name='Recipe',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('description', models.TextField(blank=True)),
                 ('title', models.CharField(max_length=255)),
+                ('description', models.TextField(blank=True)),
                 ('time_minutes', models.IntegerField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('link', models.CharField(blank=True, max_length=255)),
