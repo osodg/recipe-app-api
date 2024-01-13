276b50069b1b67b038e37bf932cba16364049ddb
Mark Fuentes <osodg@yahoo.com>
2024-01-13 09:40:19 -0500

    Still waiting for help.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,7 +11,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializes.RecipeSerializer
+    serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -19,3 +19,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for recipes."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
